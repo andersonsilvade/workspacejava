f33c7122ba1b8771a8f7f5445e907f03adf1fd23
andersonsilvade <andersonsilvade@gmail.com>
2014-08-14 11:30:55 -0300

    Android

---
+++ Workspaceandroid/MainActivity/src/br/com/k19/android/cap3/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.k19.android.cap3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	public  void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frame);
+	}
+
+	
+}
